6e4c9bcb6f158e534054ef882154f65d08902603
eldariko <eldariko15@gmail.com>
2026-01-14 21:03:10 +0200

    refactor: clean up formatting in elevator configuration

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 
 export const elevatorConfig = {
     floors: 10,           // num of floors - 0 to 9
-    elevators: 5,                   //
-    speedPerFloor: 1000,  //
-    doorWaitTime: 2000,   //
+    elevators: 5,
+    speedPerFloor: 1000,
+    doorWaitTime: 2000,
     arrivalSound: 'src/assets/elevator-ding.mp3' // path to sound file
 };
